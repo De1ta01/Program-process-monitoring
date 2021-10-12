ab55e8ff0087978f777a6b214c9b333159648853
De1ta01 <bmv.1999@mail.ru>
2021-10-13 02:21:20 +0300

    Process Monitoring

---
+++ Program process monitoring/ProcessMonitoring.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Program_process_monitoring
+{
+    partial class ProcessMonitoring
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.performanceCPU = new System.Diagnostics.PerformanceCounter();
+            this.myProcess = new System.Diagnostics.Process();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Запустить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(89, 14);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(196, 60);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Приложение (*.exe)|*.exe";
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(226, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Время мониторинга";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Текстовый файл (*.txt)|*.txt";
+            this.saveFileDialog1.Tag = "";
+            // 
+            // performanceCPU
+            // 
+            this.performanceCPU.CategoryName = "Processor";
+            this.performanceCPU.CounterName = "% Processor Time";
+            this.performanceCPU.InstanceName = "_total";
+            // 
+            // myProcess
+            // 
+            this.myProcess.StartInfo.Domain = "";
+            this.myProcess.StartInfo.LoadUserProfile = false;
+            this.myProcess.StartInfo.Password = null;
+            this.myProcess.StartInfo.StandardErrorEncoding = null;
+            this.myProcess.StartInfo.StandardOutputEncoding = null;
+            this.myProcess.StartInfo.UserName = "";
+            this.myProcess.SynchronizingObject = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Интервал сбора статистики в секундах";
+            // 
+            // ProcessMonitoring
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 142);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ProcessMonitoring";
+            this.Text = "Process monitoring";
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCPU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Diagnostics.PerformanceCounter performanceCPU;
+        private System.Diagnostics.Process myProcess;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Program process monitoring/ProcessMonitoring.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Program_process_monitoring
+{
+    public partial class ProcessMonitoring : Form
+    {
+        public ProcessMonitoring()
+        {
+            InitializeComponent();
+
+        }
+
+        private int c = 0;
+        private bool f = false;
+        private int Proc_Id = 0;
+        private string infoWS = null;
+        private string infoHC = null;
+        private string infoCPU = null;
+        private string infoPMS = null;
+        private string infoTime = null;
+        private string Proc_Dir = null;
+        private string Proc_Name = null;
+
+        private string GetProcessName() //Метод получения имени процесса из пути до исполняемого файла
+        {
+            string PrN;
+            string Proc_Path = GetProcessDir();
+            string[] subs = Proc_Path.Split('\\', '.');
+            PrN = subs[subs.Length - 2];
+            return PrN;
+        }
+
+        private string GetProcessDir() => myProcess.StartInfo.FileName; //Метод получения пути до исполняемого файла
+
+        private int GetProcessId(string PrN) //Метод получения ID запущенного процесса
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process theprocess in processlist)
+            {
+                if (theprocess.ProcessName == PrN)
+                    Proc_Id = theprocess.Id;
+            }
+            return Proc_Id;
+        }
+
+
+        private bool ProcessStart(string PrN) //Метод проверки на наличие запущенного процесса
+        {
+            Process[] processes = Process.GetProcessesByName(PrN);
+            return processes.Length != 0 ? true : false;
+        }
+
+        private void Info_Save() //Метод сохранения полученной информации
+        {
+            string infoAll = null;
+
+            infoAll = "Название процесса\n" + Proc_Name +
+                        "\n\nПуть до исполняемого файла\n" + Proc_Dir +
+                        "\n\nВремя в секундах\n" + infoTime +
+                        "\n\nНагрузка на процессор в процентах\n" + infoCPU +
+                        "\n\nОбъём физической памяти, испульзуемой процесом (Мб)\n" + infoWS +
+                        "\n\nОбъём виртаульной памяти, зарезервированной ОС для процесса (Мб)\n" + infoPMS +
+                        "\n\nЧисло дескрипторов операционной системы, открытых процессом\n" + infoHC;
+
+            Proc_Id = 0;
+            infoWS = null;
+            infoHC = null;
+            infoCPU = null;
+            infoPMS = null;
+            infoTime = null;
+
+            saveFileDialog1.FileName = Proc_Name;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                File.WriteAllText(saveFileDialog1.FileName, infoAll);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                using (myProcess = Process.GetProcessById(Proc_Id)) //Присоединение к запущенному процессу через его ID
+                {
+                    infoTime += Convert.ToString(c += Convert.ToInt32(textBox2.Text)) + "\t";
+                    myProcess.Refresh();
+                    performanceCPU.NextValue(); //Получение значения нагрузки на CPU
+                    System.Threading.Thread.Sleep(500);
+                    infoCPU += Convert.ToString(Math.Round(performanceCPU.NextValue(), 1)) + "\t"; //Получение актуального значения нагрузки на CPU
+                    infoWS += Convert.ToString(myProcess.WorkingSet64 / 1000000) + "\t"; //Получение значения используемой физической памяти
+                    infoPMS += Convert.ToString(myProcess.PrivateMemorySize64 / 1000000) + "\t"; //Получение зарезервированной виртуальной памяти
+                    infoHC += Convert.ToString(myProcess.HandleCount) + "\t"; //Получение актуального значения дескрипторов
+                    f = true;
+
+                    label1.Text = "Время: " + Convert.ToString(c) + "с";
+                }
+            }
+            catch
+            {
+                myProcess.Dispose();
+                timer1.Stop();
+
+                if (f)
+                {
+                    Info_Save();
+                }
+                else
+                {
+                    MessageBox.Show("Вы быстро закрыли процесс, программа не успела получить все данные!");
+                }
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Proc_Dir = openFileDialog1.FileName;
+                textBox1.Text = Proc_Dir;
+                label1.Text = "Время: 0с";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            timer1.Interval = Convert.ToInt32(textBox2.Text) * 1000;
+            c = 0;
+
+            myProcess = new Process();
+            myProcess.StartInfo.FileName = Proc_Dir;
+
+            if (!File.Exists(Proc_Dir)) //Проверка на наличие файла
+            {
+                MessageBox.Show("Вы не выбрали путь до исполняемого файла!");
+                button1.Enabled = true;
+                button2.Enabled = true;
+                return;
+            }
+
+            Proc_Name = GetProcessName();
+            try
+            {
+                if (ProcessStart(Proc_Name))
+                {
+                    Proc_Id = GetProcessId(Proc_Name);
+                    timer1.Start();
+                }
+                else
+                {
+                    myProcess.Start();
+                    Proc_Id = myProcess.Id;
+                    timer1.Start();
+                }
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                MessageBox.Show("Вы запустили приложение с повышенными правами и не " +
+                    "предоставили разрешение на внесение изменений на устройстве!");
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+    }
+}
